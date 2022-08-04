ca43aefc61db996bb28883dd4432ad94db6d35dd
Sidnei Cerqueira <sidnei.c.rodrigues@hotmail.com>
2022-08-04 18:31:22 -0300

    base criada

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # nodets-adot1cao
+Projeto feito usando Node + Typescript
+
+### Pré-requisitos globais:
+`npm install -g nodemon typescript ts-node`
+
+### Instalação:
+`npm install`
+
+### Para rodar o projeto:
+`npm run start-dev`
---
+++ src/server.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import mustache from 'mustache-express';
+import path, { dirname } from 'path';
+import mainRoutes from './routes/index';
+
+dotenv.config();
+
+const server = express();
+// CONFIGURAÇAO DO TEMPLATE ENGINE MUSTACHE
+server.set('view engine', 'mustache');
+server.set('views', path.join(__dirname, 'views'));
+server.engine('mustache', mustache())
+// CONFIGURAÇÃO DA PASTA PUBLICA
+server.use(express.static(path.join(__dirname, '../public')));
+
+// CONFIGURAÇÕES DAS ROTAS
+server.use('/', mainRoutes);
+
+// CONFIGURAÇÃO DE PÁGINA NÃO ENCONTRADA
+server.use((req, res)=>{
+    res.send('Página não encontrada')
+})
+
+// CONFIGURAÇÃO DE ESCUTA DO SERVIDOR WEB
+server.listen(process.env.PORT);
+console.log(`Rodando na porta: ${process.env.PORT}`);
